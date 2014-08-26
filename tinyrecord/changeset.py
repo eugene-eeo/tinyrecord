2dcb9641b8534705886c38dea3106e827478f341
Eugene Eeo <xe0jun@hotmail.com>
2014-08-26 21:16:12 +0800

    use context manager

--- tinyrecord/changeset.py
+++ tinyrecord/changeset.py
@@ -1,18 +1,26 @@
+from contextlib import contextmanager
+
+
 class Changeset(object):
     def __init__(self, database):
         self.db = database
         self.record = []
 
-    def execute(self):
+    @contextmanager
+    def context(self):
         data = self.db._read()
-        for operation in self.record:
-            operation.perform(data)
+        yield data
         self.db._write(data)
         if data:
             last_id = max(data)
             db_last = self.db._last_id
             if db_last < last_id:
                 self.db._last_id = last_id
+
+    def execute(self):
+        with self.context() as data:
+            for operation in self.record:
+                operation.perform(data)
 
     def append(self, change):
         self.record.append(change)
