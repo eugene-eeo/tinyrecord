fc9277c2a993251345b99f256d169d0c937ec378
Eugene Eeo <xe0jun@hotmail.com>
2014-08-27 14:28:07 +0800

    add docstrings

--- tinyrecord/changeset.py
+++ tinyrecord/changeset.py
@@ -2,6 +2,13 @@ from contextlib import contextmanager
 
 
 class Changeset(object):
+    """
+    A changeset represents a series of changes that
+    can be applied to the *database*.
+
+    :param database: The TinyDB table.
+    """
+
     def __init__(self, database):
         self.db = database
         self.record = []
@@ -13,6 +20,11 @@ class Changeset(object):
     @property
     @contextmanager
     def data(self):
+        """
+        Returns the data from reading the database
+        and updates the database's last_id properly,
+        as well as clearing the cache on exit.
+        """
         data = self.db._read()
         yield data
         self.db._write(data)
@@ -22,12 +34,28 @@ class Changeset(object):
             self.db._last_id = d1 if d1 > d2 else d2
 
     def execute(self):
+        """
+        Execute the changeset, applying every
+        operation on the database. Note that this
+        function is not idempotent, if you call
+        it again and again it will be executed
+        many times.
+        """
         with self.data as data:
             for operation in self.record:
                 operation.perform(data)
 
     def append(self, change):
+        """
+        Append a *change* to the internal record
+        of operations.
+
+        :param change: The change to append.
+        """
         self.record.append(change)
 
     def clear(self):
+        """
+        Clear the internal record.
+        """
         del self.record[:]
--- tinyrecord/operations.py
+++ tinyrecord/operations.py
@@ -4,6 +4,13 @@ class Operation(object):
 
 
 class InsertMultiple(Operation):
+    """
+    Insert multiple records *iterable* into the
+    database.
+
+    :param iterable: The iterable of elements to
+        be inserted into the DB.
+    """
     def __init__(self, iterable):
         self.iterable = iterable
 
@@ -15,23 +22,45 @@ class InsertMultiple(Operation):
 
 
 class Insert(Operation):
+    """
+    Insert a single record into the database.
+    An insert is just a special case of the
+    ``InsertMultiple`` where you insert a
+    one element tuple of the element.
+
+    :param element: The element to insert.
+    """
     def __new__(self, element):
         return InsertMultiple((element,))
 
 
 class Update(Operation):
-    def __init__(self, update, query):
-        self.update = update
+    """
+    Update the records in the database that
+    match a certian *query* with the *fields*.
+
+    :param fields: The fields to update.
+    :param query: Update all documents
+        matching this query.
+    """
+    def __init__(self, fields, query):
+        self.fields = fields
         self.query = query
 
     def perform(self, data):
         for key in data:
             value = data[key]
             if self.query(value):
-                value.update(self.update)
+                value.update(self.fields)
 
 
 class Remove(Operation):
+    """
+    Remove documents from the DB matching
+    the given *query*.
+
+    :param query: The query to remove.
+    """
     def __init__(self, query):
         self.query = query
 
