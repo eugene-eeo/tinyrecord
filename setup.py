8c59a59896c4fc23f0dc2e180f34ff646bf30203
Eugene Eeo <xe0jun@hotmail.com>
2014-09-05 17:36:24 +0800

    new setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tinydb',
+    version='2.0.0',
+    packages=find_packages(),
+
+    zip_safe=True,
+    author='Eugene Eeo',
+    author_email='packwolf58@gmail.com',
+    description='Atomic transactions for TinyDB',
+    license='MIT',
+    keywords='tinydb nosql database transaction',
+    url='https://github.com/eugene-eeo/tinyrecord',
+)
