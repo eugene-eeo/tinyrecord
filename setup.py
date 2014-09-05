6b7e15656a730229935b7ecde31545a0bc2caf03
Eugene Eeo <xe0jun@hotmail.com>
2014-09-05 17:37:57 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tinydb',
-    version='2.0.0',
+    name='tinyrecord',
+    version='0.1.0',
     packages=find_packages(),
 
     zip_safe=True,
